ad7f5955483d74bfab4cb32bc2259137b841ae54
Jackie M <jackiemettler@yahoo.com>
2025-03-16 11:51:22 -0400

    Homework 4. step 1: Updated Target product search test case and added Behave variables

--- ID and XPATH locators.py
+++ ID and XPATH locators.py
@@ -42,3 +42,36 @@ driver.find_element(By.XPATH, "//a[@class='nav-a  ' and text()='Best Sellers']")
 driver.find_element(By.XPATH, "//h2[contains(text(), 'Luxury')]")
 # partial attr match
 driver.find_element(By.XPATH, "//select[contains(@class, 'nav-search-dropdown')]")
+
+###############################################################################################################
+###CSS SELECTORS###
+
+# CSS, by ID => #
+driver.find_element(By.CSS_SELECTOR, '#twotabsearchtextbox') # (By.ID, 'twotabsearchtextbox')
+# CSS, by ID and tag
+driver.find_element(By.CSS_SELECTOR, 'input#twotabsearchtextbox')
+
+# CSS, class => .
+driver.find_element(By.CSS_SELECTOR, '.icp-nav-flag-us')
+driver.find_element(By.CSS_SELECTOR, '.icp-nav-flag-us.icp-nav-flag')
+driver.find_element(By.CSS_SELECTOR, '.icp-nav-flag.icp-nav-flag-us')
+# CSS, class and tag
+driver.find_element(By.CSS_SELECTOR, 'span.icp-nav-flag.icp-nav-flag-us')
+# CSS, tag, id, class
+driver.find_element(By.CSS_SELECTOR, "input#twotabsearchtextbox.nav-progressive-attribute")
+
+# CSS, attributes => []
+driver.find_element(By.CSS_SELECTOR, "[aria-label='Search Amazon']")
+driver.find_element(By.CSS_SELECTOR, "[name='field-keywords']")
+driver.find_element(By.CSS_SELECTOR, "[name='field-keywords'][aria-label='Search Amazon']")
+driver.find_element(By.CSS_SELECTOR, "input[name='field-keywords'][aria-label='Search Amazon']")
+
+driver.find_element(By.CSS_SELECTOR, ".nav-input[name='field-keywords'][aria-label='Search Amazon']")
+driver.find_element(By.CSS_SELECTOR, "input.nav-input[name='field-keywords'][aria-label='Search Amazon']")
+
+# CSS, attributes, contains => *= []
+driver.find_element(By.CSS_SELECTOR, "[aria-label*='Amazon']")
+driver.find_element(By.CSS_SELECTOR, "[data-test='accountNav-signIn']")
+driver.find_element(By.CSS_SELECTOR, "[class*='styles_ndsBaseButton'][class*='styles_ndsButtonPrimary']")
+
+# Note: CSS cannot connect to text()
