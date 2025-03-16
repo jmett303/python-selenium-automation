6a620e21a62b55423ac56dcf1d560c5f85d0c804
Jackie M <jackiemettler@yahoo.com>
2025-03-16 12:19:18 -0400

    homework 4. step 2: Created Target Circle Test Case to verify benefit cell count

---
+++ features/steps/target_circle_steps.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep, process_time_ns
+
+
+#locators
+BENEFIT_CELLS = (By.CSS_SELECTOR, '.cell-item-content')
+
+
+@given('Open target circle page')
+def open_target_circle(context):
+    context.driver.get('https://www.target.com/circle')
+
+@then('Verify at least {cell_amt} benefit cells shown')
+def verify_benefit_cells(context, cell_amt):
+    cell_amt = int(cell_amt)
+    benefit_cells = context.driver.find_elements(*BENEFIT_CELLS)
+    assert len(benefit_cells) >= 10, f'Expected {cell_amt} benefit cells, but got {len(benefit_cells)}'
+    print(f'Number of benefit cells found = {len(benefit_cells)}')
---
+++ features/tests/target_circle.feature
@@ -0,0 +1,6 @@
+# Created by jacki at 3/16/2025
+Feature: Target circle test cases
+
+  Scenario: Verify at least 10 benefit cells shown
+    Given Open target circle page
+    Then Verify at least 10 benefit cells shown
