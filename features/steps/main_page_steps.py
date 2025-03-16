b8a5f1c1dd8b6f4fa81098a3af50de6a69a30668
Jackie M <jackiemettler@yahoo.com>
2025-03-16 11:50:59 -0400

    Homework 4. step 1: Updated Target product search test case and added Behave variables

--- features/steps/main_page_steps.py
+++ features/steps/main_page_steps.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
-from time import sleep
+from time import sleep, process_time_ns
 
 #Locators
 SEARCH_FIELD = (By.ID, 'search')
 SEARCH_BUTTON = (By.XPATH, "//button[@data-test='@web/Search/SearchButton']")
 CART_ICON = (By.CSS_SELECTOR, "[data-test='@web/CartLink']")
+HEADER_LINKS = (By.CSS_SELECTOR, '[data-test*="UtilityHeader"]')
 SIGN_IN = (By.CSS_SELECTOR, '[data-test="@web/AccountLink"]')
 SIGN_IN_2 = (By.CSS_SELECTOR, '[data-test="accountNav-signIn"]')
 
@@ -32,7 +33,16 @@ def click_sign_in(context):
     context.driver.find_element(*SIGN_IN).click()
     sleep(1)
 
+
 @when('Click 2nd sign in')
 def click_2nd_sign_in(context):
     context.driver.find_element(*SIGN_IN_2).click()
     sleep(1)
+
+
+@then('Verify {link_amount} header links are shown')
+def verify_6_header_links_shown(context, link_amount):
+    link_amount = int(link_amount)
+    links = context.driver.find_elements(*HEADER_LINKS)
+    assert len(links) == 6, f'Expected {link_amount} header links, but got {len(links)}'
+    print(links)
---
+++ features/tests/main_page_ui.feature
@@ -0,0 +1,6 @@
+# Created by jacki at 3/16/2025
+Feature: Main page ui test
+
+  Scenario: Verify header links are shown
+    Given  Open target main page
+    Then Verify 6 header links are shown
